e4c382bf6b157683951b831c3937e05626aa4130
islxyqwe <islxyqwe123@gmail.com>
2023-10-23 18:04:29 +0800

    feat: paint demo

--- packages/graphic-walker/src/App.tsx
+++ packages/graphic-walker/src/App.tsx
@@ -30,6 +30,7 @@ import { ErrorContext } from './utils/reportError';
 import { ErrorBoundary } from 'react-error-boundary';
 import Errorpanel from './components/errorpanel';
 import { useCurrentMediaTheme } from './utils/media';
+import Painter from './components/painter';
 import { parseErrorMessage } from './utils';
 import { VizEmbedMenu } from './components/embedMenu';
 import DataBoard from './components/dataBoard';
@@ -150,6 +151,7 @@ export const VizApp = observer(function VizApp(props: BaseVizProps) {
                                     <Errorpanel />
                                     <LogPanel />
                                     <BinPanel />
+                                    <Painter />
                                     {vizStore.showGeoJSONConfigPanel && <GeoConfigPanel geoList={props.geoList} />}
                                     <div className="sm:flex">
                                         <SideReisze
