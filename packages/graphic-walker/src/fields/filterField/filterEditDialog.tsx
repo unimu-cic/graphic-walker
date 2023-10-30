7bab0ad302862529016e3e02b6f0554acb9686b7
islxyqwe <islxyqwe123@gmail.com>
2023-10-30 11:07:41 +0800

    fix: lock the paint field

--- packages/graphic-walker/src/fields/filterField/filterEditDialog.tsx
+++ packages/graphic-walker/src/fields/filterField/filterEditDialog.tsx
@@ -9,7 +9,7 @@ import Tabs, { RuleFormProps } from './tabs';
 import DefaultButton from '../../components/button/default';
 import PrimaryButton from '../../components/button/primary';
 import DropdownSelect from '../../components/dropdownSelect';
-import { COUNT_FIELD_ID, MEA_KEY_ID, MEA_VAL_ID } from '../../constants';
+import { COUNT_FIELD_ID, MEA_KEY_ID, MEA_VAL_ID, PAINT_FIELD_ID } from '../../constants';
 
 const QuantitativeRuleForm: React.FC<RuleFormProps> = ({ rawFields, field, onChange }) => {
     return <Tabs field={field} onChange={onChange} tabs={['range', 'one of']} rawFields={rawFields} />;
